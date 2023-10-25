4d005be02521caf50917550e61a787fb99a8018f
codeStackr <rdzaman20000@gmail.com>
2023-10-25 18:33:15 +0600

    Fix issues

--- main.rb
+++ main.rb
@@ -3,14 +3,16 @@ require_relative 'student'
 require_relative 'teacher'
 
 # Create a person
-person = Person.new(age: 20, name: 'Alice', parent_permission: true)
+# person = Person.new(age: 20, name: 'Alice', parent_permission: true)
+person = Person.new(28, parent_permission: false)
+p person.name
 puts person.can_use_services?
 
 # Create a student
-student = Student.new('Math Class', age: 17, name: 'Bob', parent_permission: false)
+student = Student.new('Math Class', 17, name: 'Bob', parent_permission: false)
 puts student.can_use_services?
 puts student.play_hooky
 
 # Create a teacher
-teacher = Teacher.new('Mathematics', age: 30, name: 'Mr. Smith', parent_permission: true)
+teacher = Teacher.new('Mathematics', 30, name: 'Mr. Smith', parent_permission: true)
 puts teacher.can_use_services?
--- student.rb
+++ student.rb
@@ -3,8 +3,8 @@ require_relative 'person'
 class Student < Person
   attr_accessor :classroom
 
-  def initialize(classroom, age:, name: 'Unknown', parent_permission: true)
-    super(age: age, name: name, parent_permission: parent_permission)
+  def initialize(classroom, age, name: 'Unknown', parent_permission: true)
+    super(age, name: name, parent_permission: parent_permission)
     @classroom = classroom
   end
 
