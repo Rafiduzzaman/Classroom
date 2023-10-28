21bdd7733537879abcd19a8ae30589c2b3ec1092
codeStackr <rdzaman20000@gmail.com>
2023-10-28 22:01:32 +0600

    Fix issues

--- student.rb
+++ student.rb
@@ -3,16 +3,17 @@ require_relative 'person'
 class Student < Person
   attr_accessor :classroom
 
-  def initialize(classroom, age, name: 'Unknown', parent_permission: true)
-    super(age, name: name, parent_permission: parent_permission)
+  def initialize(classroom, age, name = 'Unknown', parent_permission: true)
+    super(age, name, parent_permission: parent_permission)
+    @classroom = classroom
+  end
+
+  def add_classroom(classroom)
+    classroom.students.push(self) unless classroom.students.include?(self)
     @classroom = classroom
   end
 
   def play_hooky
     '¯\\(ツ)/¯'
-  end
-
-  def add_to_classroom(classroom)
-    self.classroom = classroom
   end
 end
