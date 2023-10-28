f1dfaf57ef420d221284482c7ca9be826f4e373e
codeStackr <rdzaman20000@gmail.com>
2023-10-28 23:57:04 +0600

    Fix issues with date

--- book.rb
+++ book.rb
@@ -1,6 +1,7 @@
+require_relative 'rental'
+
 class Book
-  attr_accessor :title, :author
-  attr_reader :rentals
+  attr_accessor :title, :author, :rentals
 
   def initialize(title, author)
     @title = title
@@ -8,7 +9,7 @@ class Book
     @rentals = []
   end
 
-  def add_rental(person, date)
-    Rental.new(self, person, date)
+  def rent(person, date)
+    Rental.new(date, person, self)
   end
 end
