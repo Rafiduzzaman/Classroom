c3e56b7f8610066a0ad8cd2b77c4efe46ee99e97
codeStackr <rdzaman20000@gmail.com>
2023-11-02 03:18:25 +0600

    Update rentals file

--- rental.rb
+++ rental.rb
@@ -8,4 +8,32 @@ class Rental
     person.rentals << self
     book.rentals << self
   end
+
+  class Rental
+    attr_accessor :date, :person, :book
+
+    def initialize(date, person, book)
+      @date = date
+      @person = person
+      @book = book
+      person.rentals << self
+      book.rentals << self
+    end
+
+    def to_json(people, books)
+      {
+        'date' => @date,
+        'person_index' => people.index(@person),
+        'book_index' => books.index(@book)
+      }
+    end
+
+    def self.from_json(data, people, books)
+      date = data['date']
+      person = people[data['person_index']]
+      book = books[data['book_index']]
+
+      new(date, person, book)
+    end
+  end
 end
