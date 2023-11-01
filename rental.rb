5b682cc2021eecfd9df6fae8ef09c6b34f7b553c
codeStackr <rdzaman20000@gmail.com>
2023-11-02 03:34:24 +0600

    Fix rental file

--- rental.rb
+++ rental.rb
@@ -36,4 +36,20 @@ class Rental
       new(date, person, book)
     end
   end
+
+  def to_json(people, books)
+    {
+      'date' => @date,
+      'person_index' => people.index(@person),
+      'book_index' => books.index(@book)
+    }
+  end
+
+  def self.from_json(data, people, books)
+    date = data['date']
+    person = people[data['person_index']]
+    book = books[data['book_index']]
+
+    new(date, person, book)
+  end
 end
