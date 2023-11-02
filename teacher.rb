e237d533b762d5f4e78804356dd8b59517b957d1
codeStackr <rdzaman20000@gmail.com>
2023-11-03 03:53:28 +0600

    fix linters

--- teacher.rb
+++ teacher.rb
@@ -1,7 +1,7 @@
 require_relative 'person'
 
 class Teacher < Person
-  attr_reader :specialization 
+  attr_reader :specialization
 
   def initialize(specialization, age, name: 'Unknown', parent_permission: true)
     super(age, name, parent_permission: parent_permission)
